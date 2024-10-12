2d37b353564d6edcbbbbd59ae23cf1307412b159
ThanhSonn <phamduythanh31052005@gmail.com>
2024-10-12 10:28:18 +0700

    Helloo

--- Modau.js
+++ Modau.js
@@ -1,1 +1,8 @@
 console.log(`Hello World`);
+console.log(`My name Thanh Son`);
+// window.alert(`Hi !!!`); cửa sổ thông báo
+
+document.getElementById("myh1").textContent = `Hello`;  // Thay đổi nd html
+document.getElementById("myP").textContent = `Love you`;
+
+
